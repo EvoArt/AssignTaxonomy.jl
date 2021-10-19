3140aa276fc6560b50f2d6b7ae71611782ffd234
Documenter.jl <documenter@juliadocs.github.io>
2021-10-19 12:25:46 +0000

    build based on e6d0bda

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AssignTaxonomy","category":"page"},{"location":"#AssignTaxonomy","page":"Home","title":"AssignTaxonomy","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AssignTaxonomy.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AssignTaxonomy]","category":"page"}]
+}
