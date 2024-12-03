1b572947ed5eba5302d26bee7994aa3814d761f2
Arthur <arn203@exeter.ac.uk>
2024-12-03 10:42:05 +0000

    export struct and struct funcs

--- src/AssignTaxonomy.jl
+++ src/AssignTaxonomy.jl
@@ -1,8 +1,8 @@
 module AssignTaxonomy
 
-using FASTX, BioSequences, VectorizedKmers, Tables,PrettyTables
+using FASTX, BioSequences, VectorizedKmers, Tables, PrettyTables
 
 include("utils.jl")
 include("classifier.jl")
-export assign_taxonomy,get_reference,get_targets
+export assign_taxonomy, get_reference, get_targets, names, values, ClassificationResult
 end
